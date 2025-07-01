db249dc57d0d8c0f71216de12ff0f3b8be0612c9
Huseen <136820327+hmunye@users.noreply.github.com>
2025-07-01 12:40:06 -0400

    [linux] updated colorscheme

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -62,26 +62,93 @@ end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
     {
-        "blazkowolf/gruber-darker.nvim",
-        name = "gruber-darker",
+        "llama.nvim",
+        dir = os.getenv("HOME") .. "/Developer/llama.nvim",
+        dependencies = {},
         config = function()
-            require("gruber-darker").setup({
-                bold = true,
-                invert = {
-                    signs = false,
-                    tabline = false,
-                    visual = false,
+            require("llama").setup({
+                model = "gemma3:12b",
+                model_options = {
+                    mirostat = 0,
+                    mirostat_eta = 0.1,
+                    mirostat_tau = 5.0,
+                    num_ctx = 8192,
+                    repeat_last_n = 64,
+                    repeat_penalty = 1.1,
+                    temperature = 0.7,
+                    seed = 0,
+                    stop = nil,
+                    num_predict = -1,
+                    top_k = 40,
+                    top_p = 0.9,
+                    min_p = 0.0,
                 },
-                italic = {
-                    strings = false,
-                    comments = false,
-                    operators = false,
-                    folds = false,
+                system_message = [[]],
+                stream = true,
+                chat = {
+                    position = "right",
+                    width = 45.0,
+                    title = "",
+                    title_position = "center",
+                    border = "none",
+                    spinner_color = "#FFFFFF",
                 },
-                undercurl = true,
-                underline = false,
+                prompt = {
+                    position = "bottom",
+                    border = "rounded",
+                    start_insert_mode = true,
+                    highlight_color = "#353535",
+                },
+                keymaps = {
+                    LlamaChat = {
+                        mode = { "n" },
+                        lhs = "<C-c>",
+                    },
+
+                    LlamaSubmitPrompt = {
+                        mode = { "n", "i" },
+                        lhs = "<CR>",
+                    },
+                },
             })
-            vim.cmd("colorscheme gruber-darker")
+        end,
+    },
+    {
+        "vague2k/vague.nvim",
+        name = "rose-pine-mute",
+        config = function()
+            require("vague").setup({
+                transparent = true,
+                style = {
+                    boolean = "none",
+                    number = "none",
+                    float = "none",
+                    error = "none",
+                    comments = "none",
+                    conditionals = "none",
+                    functions = "none",
+                    headings = "none",
+                    operators = "none",
+                    strings = "none",
+                    variables = "none",
+                    keywords = "none",
+                    keyword_return = "none",
+                    keywords_loop = "none",
+                    keywords_label = "none",
+                    keywords_exception = "none",
+                    builtin_constants = "none",
+                    builtin_functions = "none",
+                    builtin_types = "none",
+                    builtin_variables = "none",
+                },
+                colors = {
+                    func = "#bc96b0",
+                    keyword = "#787bab",
+                    string = "#d4bd98",
+                    number = "#8f729e",
+                },
+            })
+            vim.cmd("colorscheme vague")
             vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
             vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
             vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
