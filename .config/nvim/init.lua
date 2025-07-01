bb1f439f4d169e9031dbffa4f9fbbac9ff024f39
Huseen <136820327+hmunye@users.noreply.github.com>
2025-07-01 12:43:12 -0400

    [linux] updated neovim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -176,6 +176,34 @@ require("lazy").setup({
         end,
     },
     {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = {},
+                sync_install = false,
+                auto_install = true,
+                indent = { enable = true },
+                highlight = {
+                    enable = true,
+                    disable = function(_, buf)
+                        local max_filesize = 100 * 1024 -- 100 KB
+                        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                        if ok and stats and stats.size > max_filesize then
+                            vim.notify(
+                                "File larger than 100KB treesitter disabled for performance",
+                                vim.log.levels.WARN,
+                                { title = "Treesitter" }
+                            )
+                            return true
+                        end
+                    end,
+                    additional_vim_regex_highlighting = { "markdown" },
+                },
+            })
+        end,
+    },
+    {
         'stevearc/oil.nvim',
         opts = {},
         config = function()
